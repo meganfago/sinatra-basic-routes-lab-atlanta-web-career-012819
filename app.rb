f9119d567d968af2b78dfca44f255b2aa6f61ecc
Megan Fago <meganlaynefago@gmail.com>
2019-02-18 18:53:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Megan."
+  end
+  
+  get '/hometown' do
+    "My hometown is Chattanooga."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is A Million Reasons."
+  end
+  
 end
